2f31fce46947c1e489619ce22c5fd7f9acfa45e2
ccostello0318 <mcchriscost0318@gmail.com>
2026-03-12 10:45:33 -0400

    Update Benchmark_modeling.R
    Added functions to find several log likelihoods for Normal, T, and Cauchy. for GARCH is WIP

--- bayes_stochastic_volatility/Benchmark_modeling.R
+++ bayes_stochastic_volatility/Benchmark_modeling.R
@@ -1,49 +1,45 @@
+library(MASS)
+library(rugarch)
+
 # Lets works from SPY
 #We already know log return volatility isnt normal.
-mu_hat <- mean(SPY_log_return)
-sigma_hat <- sd(SPY_log_return)
-print(mu_hat)
-print(sigma_hat)
-loglik_norm <- sum(dnorm(SPY_log_return,
-                         mean = mu_hat,
-                         sd = sigma_hat,
-                         log = TRUE))
-loglik_norm
+normalModelLikelihood <- function(logReturns) {
+  mu_hat <- mean(logReturns)
+  sigma_hat <- sd(logReturns)
+  loglik_norm <- sum(dnorm(logReturns,
+                           mean = mu_hat,
+                           sd = sigma_hat,
+                           log = TRUE))
+  return(loglik_norm)
+}
 
+tModelLikelihood <- function(logReturns) {
+  fit_t <- fitdistr(logReturns, "t")
+  
+  mu_t <- fit_t$estimate["m"]
+  sigma_t <- fit_t$estimate["s"]
+  nu_t <- fit_t$estimate["df"]
+  
+  loglik_t <- sum(dt((logReturns - mu_t)/sigma_t,
+                     df = nu_t,
+                     log = TRUE) - log(sigma_t))
+  return(loglik_t)
+}
 
-library(MASS)
-fit_t <- fitdistr(SPY_log_return, "t")
+cauchyModelLikelihood <- function(logReturns) {
+  fit_cauchy <- fitdistr(logReturns, "cauchy")
+  
+  mu_c <- fit_cauchy$estimate["location"]
+  gamma_c <- fit_cauchy$estimate["scale"]
+  
+  loglik_cauchy <- sum(dcauchy(logReturns,
+                               location = mu_c,
+                               scale = gamma_c,
+                               log = TRUE))
+  return(loglik_cauchy)
+}
 
-fit_t
-mu_t <- fit_t$estimate["m"]
-sigma_t <- fit_t$estimate["s"]
-nu_t <- fit_t$estimate["df"]
-
-loglik_t <- sum(dt((SPY_log_return - mu_t)/sigma_t,
-                   df = nu_t,
-                   log = TRUE) - log(sigma_t))
-
-fit_cauchy <- fitdistr(SPY_log_return, "cauchy")
-
-mu_c <- fit_cauchy$estimate["location"]
-gamma_c <- fit_cauchy$estimate["scale"]
-
-loglik_cauchy <- sum(dcauchy(SPY_log_return,
-                             location = mu_c,
-                             scale = gamma_c,
-                             log = TRUE))
-
-loglik_cauchy
-#load package to run a garch analysis
-library(rugarch)
-garch_spec_norm <- ugarchspec(
-  variance.model = list(model = "sGARCH",
-                        garchOrder = c(1,1)),
-  mean.model = list(armaOrder = c(0,0),
-                    include.mean = TRUE),
-  distribution.model = "norm"
-)
-#fit the garch model
+# garch analysis, AR1 model
 garch_fit_norm <- ugarchfit(
   spec = garch_spec_norm,
   data = SPY_log_return
@@ -68,4 +64,5 @@ garch_vol <- sigma(garch_fit_t)
 
 plot(garch_vol,
      main="Estimated Volatility (GARCH-t)",
+
      col="blue")
